11bb81c761cd5b90f7a7576f7d427a52d1ca45ac
Brandon <bnwroble@ncsu.edu>
2023-09-16 13:49:18 -0400

    [Implementation: custom exception class]

---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_scheduler.course;
+
+/**
+ * A custom exception class to represent scheduling conflicts between activities.
+ * This exception is thrown when a scheduling conflict is detected.
+ */
+public class ConflictException extends Exception {
+
+}
