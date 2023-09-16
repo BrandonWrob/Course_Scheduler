358fe1eed50e83f0fe01d9882e56246a200c9c37
Brandon <bnwroble@ncsu.edu>
2023-09-16 14:01:05 -0400

    [Implementation] Constructors

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
@@ -1,12 +1,28 @@
-/**
- * 
- */
 package edu.ncsu.csc216.wolf_scheduler.course;
 
 /**
  * A custom exception class to represent scheduling conflicts between activities.
  * This exception is thrown when a scheduling conflict is detected.
+ * @author Brandon Wroblewski
  */
 public class ConflictException extends Exception {
 
+    /** ID used for serialization. */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a new ConflictException with a default message: "Schedule conflict."
+     */
+    public ConflictException() {
+        super("Schedule conflict.");
+    }
+
+    /**
+     * Constructs a new ConflictException with a specified message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the getMessage() method)
+     */
+    public ConflictException(String message) {
+        super(message);
+    }
 }
