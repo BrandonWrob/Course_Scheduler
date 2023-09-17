6629c6fdaec9bda2aa67a8774f7f3411f6d6404d
Brandon <bnwroble@ncsu.edu>
2023-09-16 21:07:15 -0400

    [Test] Updated test to handle conflicts

--- WolfScheduler/test/edu/ncsu/csc216/wolf_scheduler/scheduler/WolfSchedulerTest.java
+++ WolfScheduler/test/edu/ncsu/csc216/wolf_scheduler/scheduler/WolfSchedulerTest.java
@@ -319,7 +319,7 @@ public class WolfSchedulerTest {
 		//Name, section, title
 		assertTrue(ws.addCourseToSchedule(NAME, SECTION));
 		assertTrue(ws.addCourseToSchedule("CSC 226", "001"));
-		ws.addEventToSchedule("Lunch", "MWF", 1200, 1300, "Food");
+		ws.addEventToSchedule("Lunch", "MWF", 1500, 1600, "Food");
 		assertTrue(ws.addCourseToSchedule("CSC 116", "002"));
 		
 		String [][] schedule = ws.getScheduledActivities();
@@ -337,7 +337,7 @@ public class WolfSchedulerTest {
 		assertEquals("", schedule[2][0]);
 		assertEquals("", schedule[2][1]);
 		assertEquals("Lunch", schedule[2][2]);
-		assertEquals("MWF 12:00PM-1:00PM", schedule[2][3]);
+		assertEquals("MWF 3:00PM-4:00PM", schedule[2][3]);
 		//Row 3
 		assertEquals("CSC 116", schedule[3][0]);
 		assertEquals("002", schedule[3][1]);
@@ -356,9 +356,8 @@ public class WolfSchedulerTest {
 		//Name, section, title, credits, instructor id, meeting string
 		assertTrue(ws.addCourseToSchedule(NAME, SECTION));
 		assertTrue(ws.addCourseToSchedule("CSC 226", "001"));
-		ws.addEventToSchedule("Lunch", "MWF", 1200, 1300, "Food");
+		ws.addEventToSchedule("Lunch", "MWF", 1500, 1600, "Food");
 		assertTrue(ws.addCourseToSchedule("CSC 116", "002"));
-		
 		String [][] schedule = ws.getFullScheduledActivities();
 		//Row 1
 		assertEquals("CSC 216", schedule[0][0]);
@@ -382,7 +381,7 @@ public class WolfSchedulerTest {
 		assertEquals("Lunch", schedule[2][2]);
 		assertEquals("", schedule[2][3]);
 		assertEquals("", schedule[2][4]);
-		assertEquals("MWF 12:00PM-1:00PM", schedule[2][5]);
+		assertEquals("MWF 3:00PM-4:00PM", schedule[2][5]);
 		assertEquals("Food", schedule[2][6]);
 		//Row 3
 		assertEquals("CSC 116", schedule[3][0]);
@@ -392,6 +391,7 @@ public class WolfSchedulerTest {
 		assertEquals("spbalik", schedule[3][4]);
 		assertEquals("MW 11:20AM-1:10PM", schedule[3][5]);
 		assertEquals("", schedule[1][6]);
+		
 	}
 	
 	/**
@@ -454,6 +454,33 @@ public class WolfSchedulerTest {
 		} catch (IOException e) {
 			fail("Error reading files.");
 		}
+	}
+	
+	/**
+	 * Test adding courses and events with conflicts to the schedule.
+	 */
+	@Test
+	public void testAddCoursesAndEventsWithConflict() {
+	    WolfScheduler ws = new WolfScheduler(validTestFile);
+
+	    // Add a course to the schedule
+	    assertTrue(ws.addCourseToSchedule("CSC 217", "202"));
+
+	    // Try to add a course that conflicts with the existing course
+	    try {
+	        ws.addCourseToSchedule("CSC 116", "001");
+	        fail("Expected ConflictException");
+	    } catch (IllegalArgumentException e) {
+	        assertEquals("The course cannot be added due to a conflict.", e.getMessage());
+	    }
+
+	    // Try to add an event that conflicts with the existing course
+	    try {
+	        ws.addEventToSchedule("Meeting", "M", 1000, 1445, "Important meeting");
+	        fail("Expected ConflictException");
+	    } catch (IllegalArgumentException e) {
+	        assertEquals("The event cannot be added due to a conflict.", e.getMessage());
+	    }
 	}
 
 }
